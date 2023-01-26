e31edc60a95fb8e8aee9ee5419bae76a76adfb09
AlanTak19 <123612229+AlanTak19@users.noreply.github.com>
2023-01-26 12:00:50 +0600

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,132 @@
+#exercize #1
+print
+("Hello World")
+
+#exercize #2
+if 5 > 2:
+  print("Five is greater than two!")
+  
+#exercize #3
+#This is a comment
+
+#exercize #4
+"""
+This is a comment
+written in 
+more than just one line
+"""
+
+#exercize #5
+carname= "Volvo"
+
+#exercize #6
+x=50
+
+#exercize #7
+x = 5
+y = 10
+print(x + y)
+
+#exercize #8
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#exercize #9
+myfirst_name = "John"
+
+#exercize #10
+x=y=z="Orange"
+
+#exercize #11
+def myfunc():
+  global x
+  x = "fantastic"
+
+#exercize #12  
+x = 5
+print(type(x))
+
+int
+
+#exercize #13
+x = "Hello World"
+print(type(x))
+
+str
+
+#exercize #14
+x = 20.5
+print(type(x))
+
+float
+
+#exercize #15
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+list
+
+#exercize #16
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+tuple
+
+#exercize #17
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+dict
+
+#exercize #18
+x = True
+print(type(x))
+
+bool
+
+#exercize #19
+x = 5
+x = float(x)
+
+#exercize #20
+x = 5.5
+x = int(x)
+
+#exercize #21
+x = 5
+x = complex(x)
+
+#exercize #22
+x = "Hello World"
+print(len(x))
+
+#exercize #23
+txt = "Hello World"
+x = txt[0]
+
+#exercize #24
+txt = "Hello World"
+x = txt[2:5]
+
+#exercize #25
+txt = " Hello World "
+x = txt.strip()
+
+#exercize #26
+txt = "Hello World"
+txt = txt.upper()
+
+#exercize #27
+txt = "Hello World"
+txt = txt.lower()
+
+#exercize #28
+txt = "Hello World"
+txt = txt.replace("H","J")
+
+#exercize #29
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
